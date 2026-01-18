ec194c98c103f30cd114ca91d24557084016fd4f
klartz <92497219+klartz@users.noreply.github.com>
2026-01-18 11:36:46 -0300

    vasp: add libxc variant

--- repos/spack_repo/builtin/packages/vasp/package.py
+++ repos/spack_repo/builtin/packages/vasp/package.py
@@ -36,6 +36,7 @@ class Vasp(MakefilePackage, CudaPackage):
 
     variant("shmem", default=True, description="Enable use_shmem build flag")
     variant("hdf5", default=False, description="Enabled HDF5 support")
+    variant("libxc", default=False, description="Enable libxc support")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
@@ -56,6 +57,7 @@ class Vasp(MakefilePackage, CudaPackage):
     depends_on("scalapack")
     depends_on("nccl", when="+cuda")
     depends_on("hdf5+fortran+mpi", when="+hdf5")
+    depends_on("libxc", when="+libxc")
     # at the very least the nvhpc mpi seems required
     requires("^nvhpc+mpi+lapack+blas", when="%nvhpc")
 
@@ -222,6 +224,11 @@ class Vasp(MakefilePackage, CudaPackage):
             cpp_options.append("-DVASP_HDF5")
             llibs.append(spec["hdf5:fortran"].libs.ld_flags)
             incs.append(spec["hdf5"].headers.include_flags)
+
+        if spec.satisfies("+libxc"):
+            cpp_options.append("-DUSELIBXC")
+            llibs.append(spec["libxc:fortran"].libs.ld_flags)
+            incs.append(spec["libxc"].headers.include_flags)
 
         if spec.satisfies("%gcc@10:"):
             fflags.append("-fallow-argument-mismatch")
